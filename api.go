e9758171a800c0b9cb9fdb84415d528c2b8dae0f
Patrick Molgaard <patrick@gleepost.com>
2013-09-24 16:36:45 +0100

    split off sql into its own file

--- api.go
+++ api.go
@@ -119,30 +119,6 @@ func jsonResp(w http.ResponseWriter, resp []byte, code int) {
 }
 
 const (
-	ruleSelect         = "SELECT network_id, rule_type, rule_value FROM net_rules"
-	createUser         = "INSERT INTO users(name, password, email) VALUES (?,?,?)"
-	ConnectionString   = "gp:PnOaw3XzP6Tlq6fWvvVv@tcp(localhost:3306)/gleepost?charset=utf8"
-	PassSelect         = "SELECT id, password FROM users WHERE name = ?"
-	randomSelect       = "SELECT id, name FROM users ORDER BY RAND()"
-	conversationInsert = "INSERT INTO conversations (initiator, last_mod) VALUES (?, NOW())"
-	userSelect         = "SELECT id, name FROM users WHERE id=?"
-	participantInsert  = "INSERT INTO conversation_participants (conversation_id, participant_id) VALUES (?,?)"
-	postInsert         = "INSERT INTO wall_posts(`by`, `text`, network_id) VALUES (?,?,?)"
-	wallSelect         = "SELECT id, `by`, time, text FROM wall_posts WHERE network_id = ? ORDER BY time DESC LIMIT 20"
-	networkSelect      = "SELECT user_network.network_id, network.name FROM user_network INNER JOIN network ON user_network.network_id = network.id WHERE user_id = ?"
-	conversationSelect = "SELECT conversation_participants.conversation_id FROM conversation_participants JOIN conversations ON conversation_participants.conversation_id = conversations.id WHERE participant_id = ? ORDER BY conversations.last_mod DESC LIMIT ?, 20"
-	participantSelect  = "SELECT participant_id, users.name FROM conversation_participants JOIN users ON conversation_participants.participant_id = users.id WHERE conversation_id=?"
-	messageInsert      = "INSERT INTO chat_messages (conversation_id, `from`, `text`) VALUES (?,?,?)"
-	messageSelect      = "SELECT id, `from`, text, timestamp, seen FROM chat_messages WHERE conversation_id = ? ORDER BY timestamp DESC LIMIT ?, 20"
-	tokenInsert        = "INSERT INTO tokens (user_id, token, expiry) VALUES (?, ?, ?)"
-	tokenSelect        = "SELECT expiry FROM tokens WHERE user_id = ? AND token = ?"
-	conversationUpdate = "UPDATE conversations SET last_mod = NOW() WHERE id = ?"
-	commentInsert      = "INSERT INTO post_comments (post_id, `by`, text) VALUES (?, ?, ?)"
-	commentSelect      = "SELECT id, `by`, text, timestamp FROM post_comments WHERE post_id = ? ORDER BY timestamp DESC LIMIT ?, 20"
-	lastMessageSelect  = "SELECT id, `from`, text, timestamp, seen FROM chat_messages WHERE conversation_id = ? ORDER BY timestamp DESC LIMIT 1"
-	commentCountSelect = "SELECT COUNT(*) FROM post_comments WHERE post_id = ?"
-	profileSelect      = "SELECT name, `desc` FROM users WHERE id = ?"
-	MaxConnectionCount = 100
 	UrlBase            = "/api/v0.9"
 	LoginOverride      = false
 	MysqlTime          = "2006-01-02 15:04:05"
@@ -151,45 +127,8 @@ const (
 )
 
 var (
-	ruleStmt               *sql.Stmt
-	registerStmt           *sql.Stmt
-	passStmt               *sql.Stmt
-	randomStmt             *sql.Stmt
-	userStmt               *sql.Stmt
-	conversationStmt       *sql.Stmt
-	participantStmt        *sql.Stmt
-	networkStmt            *sql.Stmt
-	postStmt               *sql.Stmt
-	wallSelectStmt         *sql.Stmt
-	conversationSelectStmt *sql.Stmt
-	participantSelectStmt  *sql.Stmt
-	messageInsertStmt      *sql.Stmt
-	messageSelectStmt      *sql.Stmt
-	tokenInsertStmt        *sql.Stmt
-	tokenSelectStmt        *sql.Stmt
-	conversationUpdateStmt *sql.Stmt
-	commentInsertStmt      *sql.Stmt
-	commentSelectStmt      *sql.Stmt
-	lastMessageSelectStmt  *sql.Stmt
-	commentCountSelectStmt *sql.Stmt
-	profileSelectStmt      *sql.Stmt
 	pool                   *redis.Pool
 )
-
-func keepalive(db *sql.DB) {
-	tick := time.Tick(1 * time.Hour)
-	for {
-		<-tick
-		err := db.Ping()
-		if err != nil {
-			log.Print(err)
-			db, err = sql.Open("mysql", ConnectionString)
-			if err != nil {
-				log.Fatalf("Error opening database: %v", err)
-			}
-		}
-	}
-}
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
@@ -198,94 +137,7 @@ func main() {
 		log.Fatalf("Error opening database: %v", err)
 	}
 	db.SetMaxIdleConns(MaxConnectionCount)
-	ruleStmt, err = db.Prepare(ruleSelect)
-	if err != nil {
-		log.Fatal(err)
-	}
-	registerStmt, err = db.Prepare(createUser)
-	if err != nil {
-		log.Fatal(err)
-	}
-	passStmt, err = db.Prepare(PassSelect)
-	if err != nil {
-		log.Fatal(err)
-	}
-	randomStmt, err = db.Prepare(randomSelect)
-	if err != nil {
-		log.Fatal(err)
-	}
-	conversationStmt, err = db.Prepare(conversationInsert)
-	if err != nil {
-		log.Fatal(err)
-	}
-	userStmt, err = db.Prepare(userSelect)
-	if err != nil {
-		log.Fatal(err)
-	}
-	participantStmt, err = db.Prepare(participantInsert)
-	if err != nil {
-		log.Fatal(err)
-	}
-	postStmt, err = db.Prepare(postInsert)
-	if err != nil {
-		log.Fatal(err)
-	}
-	wallSelectStmt, err = db.Prepare(wallSelect)
-	if err != nil {
-		log.Fatal(err)
-	}
-	networkStmt, err = db.Prepare(networkSelect)
-	if err != nil {
-		log.Fatal(err)
-	}
-	conversationSelectStmt, err = db.Prepare(conversationSelect)
-	if err != nil {
-		log.Fatal(err)
-	}
-	participantSelectStmt, err = db.Prepare(participantSelect)
-	if err != nil {
-		log.Fatal(err)
-	}
-	messageInsertStmt, err = db.Prepare(messageInsert)
-	if err != nil {
-		log.Fatal(err)
-	}
-	messageSelectStmt, err = db.Prepare(messageSelect)
-	if err != nil {
-		log.Fatal(err)
-	}
-	tokenInsertStmt, err = db.Prepare(tokenInsert)
-	if err != nil {
-		log.Fatal(err)
-	}
-	tokenSelectStmt, err = db.Prepare(tokenSelect)
-	if err != nil {
-		log.Fatal(err)
-	}
-	conversationUpdateStmt, err = db.Prepare(conversationUpdate)
-	if err != nil {
-		log.Fatal(err)
-	}
-	commentInsertStmt, err = db.Prepare(commentInsert)
-	if err != nil {
-		log.Fatal(err)
-	}
-	commentSelectStmt, err = db.Prepare(commentSelect)
-	if err != nil {
-		log.Fatal(err)
-	}
-	lastMessageSelectStmt, err = db.Prepare(lastMessageSelect)
-	if err != nil {
-		log.Fatal(err)
-	}
-	commentCountSelectStmt, err = db.Prepare(commentCountSelect)
-	if err != nil {
-		log.Fatal(err)
-	}
-	profileSelectStmt, err = db.Prepare(profileSelect)
-	if err != nil {
-		log.Fatal(err)
-	}
+	prepare(db)
 	go keepalive(db)
 	pool = redis.NewPool(RedisDial, 100)
 	http.HandleFunc(UrlBase+"/login", loginHandler)
