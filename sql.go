4b49195a66ea5ce365d49bfc7497346ac314660a
Patrick Molgaard <patrick@gleepost.com>
2013-11-06 15:49:03 +0000

    suppress duplicate entry errors for likes

--- sql.go
+++ sql.go
@@ -789,9 +789,12 @@ func dbCreateNotification(ntype string, by UserId, recipient UserId, isPN bool, 
 
 func dbCreateLike(user UserId, post PostId) (err error) {
 	_, err = stmt["addLike"].Exec(post, user)
+	// Suppress duplicate entry errors
 	switch err := err.(type) {
-	default:
-		log.Printf("type: %T", err)
+	case *mysql.MySQLError:
+		if err.Number == 1062 {
+			err = nil
+		}
 	}
 	return
 }
