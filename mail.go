15315b7f5a640040507de2e8252a1de0be854571
Patrick Molgaard <patrick@gleepost.com>
2013-11-12 18:48:00 +0000

    actually include email headers

--- mail.go
+++ mail.go
@@ -13,7 +13,7 @@ func send(addr string) (err error) {
 	headers = append(headers, []byte("To: "+addr+"\r\n")...)
 	headers = append(headers, []byte("Subject: Sup\r\n")...)
 	auth := smtp.PlainAuth("", conf.Email.User, conf.Email.Pass, conf.Email.Server)
-	err = smtp.SendMail(fmt.Sprintf("%s:%d", conf.Email.Server, conf.Email.Port), auth, conf.Email.From, []string{addr}, []byte("sup"))
+	err = smtp.SendMail(fmt.Sprintf("%s:%d", conf.Email.Server, conf.Email.Port), auth, conf.Email.From, []string{addr}, append(headers, []byte("sup")...))
 	log.Println(err)
 	return
 }
