814b8c68ffba413f3aaac55069df958de4102782
Patrick Molgaard <patrick@gleepost.com>
2013-11-12 18:34:22 +0000

    test email some more

--- mail.go
+++ mail.go
@@ -9,7 +9,7 @@ import (
 func send(addrs []string) (err error) {
 	conf := GetConfig()
 	auth := smtp.PlainAuth("", conf.Email.User, conf.Email.Pass, conf.Email.Server)
-	err = smtp.SendMail(fmt.Sprintf("%s:%d", conf.Email.Server, conf.Email.Port), auth, conf.Email.From, addrs, []byte("sup"))
+	err = smtp.SendMail(fmt.Sprintf("%s:%d", conf.Email.Server, conf.Email.Port), auth, conf.Email.From, addrs, []byte("From: Gleepost <contact@gleepost.com>\r\nsup"))
 	log.Println(err)
 	return
 }
