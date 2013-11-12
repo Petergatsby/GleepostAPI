8ac827b64b531f9d0d8994da184e9d6a054d364b
Patrick Molgaard <patrick@gleepost.com>
2013-11-12 18:40:18 +0000

    better mail

--- mail.go
+++ mail.go
@@ -6,10 +6,14 @@ import (
 	"fmt"
 )
 
+
 func send(addrs []string) (err error) {
 	conf := GetConfig()
+	var headers []byte("From: Gleepost <contact@gleepost.com>\r\n")
+	headers += []byte("To: "+addrs+"\r\n")
+	headers += []byte("Subject: Sup\r\n")
 	auth := smtp.PlainAuth("", conf.Email.User, conf.Email.Pass, conf.Email.Server)
-	err = smtp.SendMail(fmt.Sprintf("%s:%d", conf.Email.Server, conf.Email.Port), auth, conf.Email.From, addrs, []byte("From: Gleepost <contact@gleepost.com>\r\nsup"))
+	err = smtp.SendMail(fmt.Sprintf("%s:%d", conf.Email.Server, conf.Email.Port), auth, conf.Email.From, addrs, []byte("sup"))
 	log.Println(err)
 	return
 }
