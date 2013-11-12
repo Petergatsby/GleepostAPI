336ccd4a67f11eac89d21117e8bbf58d161d441d
Patrick Molgaard <patrick@gleepost.com>
2013-11-12 18:21:07 +0000

    fix email

--- mail.go
+++ mail.go
@@ -3,12 +3,13 @@ package main
 import (
 	"net/smtp"
 	"log"
+	"fmt"
 )
 
 func send(addrs []string) (err error) {
 	conf := GetConfig()
 	auth := smtp.PlainAuth("", conf.Email.User, conf.Email.Pass, conf.Email.Server)
-	err = smtp.SendMail(conf.Email.Server+":"+conf.Email.Port, auth, conf.Email.From, addrs, []byte("sup"))
+	err = smtp.SendMail(fmt.Sprintf("%s:%d", conf.Email.Server, conf.Email.Port), auth, conf.Email.From, addrs, []byte("sup"))
 	log.Println(err)
 	return
 }
