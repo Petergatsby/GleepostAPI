071f4ca0e95b18c8d55e90a12bedde6c1309d8d6
Patrick Molgaard <patrick@gleepost.com>
2013-12-02 13:54:56 +0000

    c -> C

--- lib/fb.go
+++ lib/fb.go
@@ -78,7 +78,7 @@ func FacebookLogin(fbToken string) (token gp.Token, err error) {
 	if err != nil {
 		return
 	}
-	token = createAndStoreToken(userId)
+	token = CreateAndStoreToken(userId)
 	return
 }
 
