072ed9f707facb72f8e95165e72f6f31d467952c
Patrick Molgaard <patrick@gleepost.com>
2013-12-04 18:19:03 +0000

    fix websockets maybe

--- lib/pubsub.go
+++ lib/pubsub.go
@@ -6,5 +6,8 @@ import (
 )
 
 func EventSubscribe(subscriptions []string) (events gp.MsgQueue) {
+	commands := make(chan gp.QueueCommand)
+	messages := make(chan []byte)
+	events = gp.MsgQueue{Commands: commands, Messages: messages}
 	return cache.EventSubscribe(subscriptions)
 }
