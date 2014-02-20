1bcf5d187030205ee51b46ceab79ad5af651901a
Patrick Molgaard <patrick@gleepost.com>
2014-02-20 18:08:30 +0000

    fix uploading to cali

--- lib/upload.go
+++ lib/upload.go
@@ -26,10 +26,16 @@ func randomFilename(extension string) (string, error) {
 	}
 }
 
-func (api *API) getS3() (s *s3.S3) {
+func (api *API) getS3(network gp.NetworkId) (s *s3.S3) {
 	var auth aws.Auth
 	auth.AccessKey, auth.SecretKey = api.Config.AWS.KeyId, api.Config.AWS.SecretKey
-	s = s3.New(auth, aws.EUWest)
+	//1911 == Stanford.
+	//TODO: Make the bucket a property of the university / group of universities
+	if network == 1911 {
+		s = s3.New(auth, aws.USWest)
+	} else {
+		s = s3.New(auth, aws.EUWest)
+	}
 	return
 }
 
@@ -53,14 +59,19 @@ func (api *API) StoreFile(id gp.UserId, file multipart.File, header *multipart.F
 		return "", gp.APIerror{err.Error()}
 	}
 	//store on s3
-	s := api.getS3()
-	var bucket *s3.Bucket
 	networks, _ := api.GetUserNetworks(id)
-	//1911 == Stanford.
-	//TODO: Make the bucket a property of the university / group of universities
-	if len(networks) > 0 && networks[0].Id == 1911 {
-		bucket = s.Bucket("gpcali")
-	} else {
+	var s *s3.S3
+	var bucket *s3.Bucket
+	switch {
+	case len(networks) > 0:
+		s = api.getS3(networks[0].Id)
+		if networks[0].Id == 1911 {
+			bucket = s.Bucket("gpcali")
+		} else {
+			bucket = s.Bucket("gpimg")
+		}
+	default:
+		s = api.getS3(1)
 		bucket = s.Bucket("gpimg")
 	}
 	data, err := ioutil.ReadAll(file)
