422f92101bad6c716c7eb199cbe2746afb71b708
Patrick Molgaard <patrick@gleepost.com>
2014-04-05 21:47:51 +0100

    Fix SQL statement for like stats

--- lib/db/stats.go
+++ lib/db/stats.go
@@ -6,7 +6,7 @@ import (
 )
 
 func (db *DB) LikesForUserBetween(user gp.UserId, start time.Time, finish time.Time) (count int, err error) {
-	s, err := db.prepare("SELECT COUNT(*) FROM post_likes WHERE post_id IN (SELECT id FROM wall_posts WHERE `by` = ?) WHERE timestamp > ? AND timestamp < ?")
+	s, err := db.prepare("SELECT COUNT(*) FROM post_likes WHERE post_id IN (SELECT id FROM wall_posts WHERE `by` = ?) WHERE `timestamp` > ? AND `timestamp` < ?")
 	if err != nil {
 		return
 	}
