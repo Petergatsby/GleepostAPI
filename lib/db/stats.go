16f2fc9bc0f18800f0fbd00aee7ca81cd6569f14
Patrick Molgaard <patrick@gleepost.com>
2014-04-07 20:24:01 +0100

    Some cohort statistics

--- lib/db/stats.go
+++ lib/db/stats.go
@@ -40,3 +40,63 @@ func (db *DB) RsvpsForUserBetween(user gp.UserId, start time.Time, finish time.T
 	err = s.QueryRow(user, start.UTC().Format(mysqlTime), finish.UTC().Format(mysqlTime)).Scan(&count)
 	return
 }
+
+func (db *DB) UsersSignedUpBetween(start time.Time, finish time.Time) (users []gp.UserId, err error) {
+	s, err := db.prepare("SELECT id FROM users WHERE `timestamp` > ? AND `timestamp` < ?")
+	if err != nil {
+		return
+	}
+	rows, err := s.Query(start.UTC().Format(mysqlTime), finish.UTC().Format(mysqlTime))
+	if err != nil {
+		return
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var u gp.UserId
+		err = rows.Scan(&u)
+		if err != nil {
+			return
+		}
+		users = append(users, u)
+	}
+	return
+}
+
+func (db *DB) UsersVerifiedBetween(start time.Time, finish time.Time) (users []gp.UserId, err error) {
+	s, err := db.prepare("SELECT id FROM users WHERE `verified` = 1 AND `timestamp` > ? AND `timestamp` < ?")
+	rows, err := s.Query(start.UTC().Format(mysqlTime), finish.UTC().Format(mysqlTime))
+	if err != nil {
+		return
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var u gp.UserId
+		err = rows.Scan(&u)
+		if err != nil {
+			return
+		}
+		users = append(users, u)
+	}
+	return
+}
+
+func (db *DB) UsersLikedBetween(start time.Time, finish time.Time) (users []gp.UserId, err error) {
+	s, err := db.prepare("SELECT DISTINCT user_id FROM post_likes WHERE user_id IN (SELECT id FROM users WHERE `timestamp` > ? AND `timestamp` < ?)")
+	if err != nil {
+		return
+	}
+	rows, err := s.Query(start.UTC().Format(mysqlTime), finish.UTC().Format(mysqlTime))
+	if err != nil {
+		return
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var u gp.UserId
+		err = rows.Scan(&u)
+		if err != nil {
+			return
+		}
+		users = append(users, u)
+	}
+	return
+}
