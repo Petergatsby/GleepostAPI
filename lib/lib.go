e801bd618b9347d7c577b375c9bce734764ecd04
Patrick Molgaard <patrick@gleepost.com>
2014-04-25 15:03:45 +0100

    Tokens now last a year

--- lib/lib.go
+++ lib/lib.go
@@ -69,7 +69,7 @@ func createToken(userId gp.UserId) gp.Token {
 	if err != nil {
 		return (gp.Token{userId, "foo", time.Now().UTC()})
 	} else {
-		expiry := time.Now().Add(time.Duration(168) * time.Hour).UTC().Round(time.Second)
+		expiry := time.Now().AddDate(1, 0, 0).UTC().Round(time.Second)
 		token := gp.Token{userId, random, expiry}
 		return (token)
 	}
