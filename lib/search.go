81d58e57a6fbd0fec993d933c2359bfb4f982955
Patrick Molgaard <patrick@gleepost.com>
2014-05-07 22:50:41 +0100

    delint search.go

--- lib/search.go
+++ lib/search.go
@@ -7,12 +7,13 @@ import (
 	"github.com/draaglom/GleepostAPI/lib/gp"
 )
 
+//ETOOSHORT represents a search query which is too short.
 var ETOOSHORT = gp.APIerror{Reason: "Your query must be at least 2 characters long"}
 
 //UserSearchUsersInNetwork returns all the users with names beginning with first, last in netId, or ENOTALLOWED if user isn't part of this network.
 //last may be omitted but first must be at least 2 characters.
-func (api *API) UserSearchUsersInNetwork(user gp.UserId, first, last string, netId gp.NetworkId) (users []gp.User, err error) {
-	in, err := api.UserInNetwork(user, netId)
+func (api *API) UserSearchUsersInNetwork(user gp.UserId, first, last string, netID gp.NetworkId) (users []gp.User, err error) {
+	in, err := api.UserInNetwork(user, netID)
 	//I don't like the idea of people being able to look for eg. %a%
 	first = strings.Replace(first, "%", "", -1)
 	last = strings.Replace(last, "%", "", -1)
@@ -24,7 +25,7 @@ func (api *API) UserSearchUsersInNetwork(user gp.UserId, first, last string, net
 	case len(first) < 2:
 		return users, &ETOOSHORT
 	default:
-		log.Printf("Searching network %d for user %s %s\n", netId, first, last)
-		return api.db.SearchUsersInNetwork(first, last, netId)
+		log.Printf("Searching network %d for user %s %s\n", netID, first, last)
+		return api.db.SearchUsersInNetwork(first, last, netID)
 	}
 }
