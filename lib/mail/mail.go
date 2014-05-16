66970ebcb32ba32c4fa018c3cc570fc8cd4a8d29
Patrick Molgaard <patrick@gleepost.com>
2014-05-16 16:44:23 +0100

    eliminate cruft in mail module

--- lib/mail/mail.go
+++ lib/mail/mail.go
@@ -3,63 +3,59 @@ package mail
 import (
 	"fmt"
 	"log"
+	"net/mail"
 	"net/smtp"
 	"time"
 
 	"github.com/draaglom/GleepostAPI/lib/gp"
 )
 
+//a Mailer is able to send email.
 type Mailer struct {
 	config gp.EmailConfig
 }
 
-type Header struct {
-	Headers []byte
+//NewHeader generates a Header with from and date pre-populated.
+func (m *Mailer) NewHeader() mail.Header {
+	h := mail.Header{}
+	h["From"] = []string{m.config.FromHeader}
+	h["Date"] = []string{time.Now().Truncate(time.Second).UTC().String()}
+	return h
 }
 
-func (m *Mailer) NewHeader() *Header {
-	h := Header{}
-	h.Headers = []byte("From: " + m.config.FromHeader + "\r\n")
-	h.Headers = append(h.Headers, []byte("Date: "+time.Now().Truncate(time.Second).UTC().String()+"\r\n")...)
-	return &h
-}
-
+//New creates a Mailer.
 func New(config gp.EmailConfig) *Mailer {
 	return &Mailer{config: config}
 }
 
-func (h *Header) To(address string) {
-	h.Headers = append(h.Headers, []byte("To: "+address+"\r\n")...)
-	return
+func toBytes(h mail.Header) []byte {
+	var bytes []byte
+	for k, v := range h {
+		//Assuming that there will only ever be a single value for each header...
+		bytes = append(bytes, []byte(k+": "+v[0]+"\r\n")...)
+	}
+	return bytes
 }
 
-func (h *Header) HTML() {
-	h.Headers = append(h.Headers, []byte("Content-Type: text/html; charset=\"UTF-8\"\r\n")...)
-	return
-}
-
-func (h *Header) Subject(subject string) {
-	h.Headers = append(h.Headers, []byte("Subject: "+subject+"\r\n")...)
-	return
-}
-
+//Send an old fashioned (ascii) email to "to"
 func (m *Mailer) Send(to string, subject string, body string) (err error) {
 	header := m.NewHeader()
-	header.To(to)
-	header.Subject(subject)
+	header["To"] = []string{to}
+	header["Subject"] = []string{subject}
 	auth := smtp.PlainAuth("", m.config.User, m.config.Pass, m.config.Server)
-	err = smtp.SendMail(fmt.Sprintf("%s:%d", m.config.Server, m.config.Port), auth, m.config.From, []string{to}, append(header.Headers, []byte(body)...))
+	err = smtp.SendMail(fmt.Sprintf("%s:%d", m.config.Server, m.config.Port), auth, m.config.From, []string{to}, append(toBytes(header), []byte(body)...))
 	log.Println(err)
 	return
 }
 
+//SendHTML: Send, but with HTML
 func (m *Mailer) SendHTML(to string, subject string, body string) (err error) {
 	header := m.NewHeader()
-	header.HTML()
-	header.To(to)
-	header.Subject(subject)
+	header["Content-Type"] = []string{"text/html; charset=\"UTF-8\"\r\n"}
+	header["To"] = []string{to}
+	header["Subject"] = []string{subject}
 	auth := smtp.PlainAuth("", m.config.User, m.config.Pass, m.config.Server)
-	err = smtp.SendMail(fmt.Sprintf("%s:%d", m.config.Server, m.config.Port), auth, m.config.From, []string{to}, append(header.Headers, []byte(body)...))
+	err = smtp.SendMail(fmt.Sprintf("%s:%d", m.config.Server, m.config.Port), auth, m.config.From, []string{to}, append(toBytes(header), []byte(body)...))
 	log.Println(err)
 	return
 
