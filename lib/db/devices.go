e0bc30686652e79d86440513b7110481d028a138
Patrick Molgaard <patrick@gleepost.com>
2014-05-16 16:16:02 +0100

    document db/devices

--- lib/db/devices.go
+++ lib/db/devices.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/draaglom/GleepostAPI/lib/gp"
+	//mysql provides the mysql driver as an import side-effect. Not sure if it's actually necessary to import everywhere though.
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -12,6 +13,7 @@ import (
 		Device
 ********************************************************************/
 
+//AddDevice idempotently records user's ios or android device id for pushing notifications to.
 func (db *DB) AddDevice(user gp.UserID, deviceType string, deviceID string) (err error) {
 	s, err := db.prepare("REPLACE INTO devices (user_id, device_type, device_id) VALUES (?, ?, ?)")
 	if err != nil {
@@ -21,6 +23,7 @@ func (db *DB) AddDevice(user gp.UserID, deviceType string, deviceID string) (err
 	return
 }
 
+//GetDevices returns all a user's devices.
 func (db *DB) GetDevices(user gp.UserID) (devices []gp.Device, err error) {
 	s, err := db.prepare("SELECT user_id, device_type, device_id FROM devices WHERE user_id = ?")
 	if err != nil {
@@ -41,6 +44,7 @@ func (db *DB) GetDevices(user gp.UserID) (devices []gp.Device, err error) {
 	return
 }
 
+//DeleteDevice deregisters this device (if it exists).
 func (db *DB) DeleteDevice(user gp.UserID, device string) (err error) {
 	log.Printf("Deleting %d's device: %s\n", user, device)
 	s, err := db.prepare("DELETE FROM devices WHERE user_id = ? AND device_id = ?")
@@ -51,6 +55,7 @@ func (db *DB) DeleteDevice(user gp.UserID, device string) (err error) {
 	return
 }
 
+//Feedback deletes the ios device with this ID unless it has been re-registered more recently than timestamp.
 func (db *DB) Feedback(deviceID string, timestamp time.Time) (err error) {
 	s, err := db.prepare("DELETE FROM devices WHERE device_id = ? AND last_update < ? AND device_type = 'ios'")
 	r, err := s.Exec(deviceID, timestamp)
@@ -59,6 +64,7 @@ func (db *DB) Feedback(deviceID string, timestamp time.Time) (err error) {
 	return
 }
 
+//GetAllDevices returns all pushable devices on this platform. Use with caution!
 func (db *DB) GetAllDevices(platform string) (devices []gp.Device, err error) {
 	s, err := db.prepare("SELECT user_id, device_type, device_id FROM devices WHERE device_type = ?")
 	if err != nil {
