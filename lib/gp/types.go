aaa3aa39c49fb712b24e69c89b7ab3e69f8982f5
Patrick Molgaard <patrick@gleepost.com>
2014-06-18 18:45:24 +0100

    clean up core module

--- lib/gp/types.go
+++ lib/gp/types.go
@@ -1,29 +1,48 @@
+//Package gp contains the core datatypes in Gleepost.
 package gp
 
 import (
 	"time"
 )
 
+//UserID is self explanatory.
 type UserID uint64
+
+//NetworkID is the id of a network (which Groups are a subset of).
 type NetworkID uint64
+
+//MessageID uniquely identifies a chat message.
 type MessageID uint64
+
+//PostID uniquely identifies a post (which Events are a subset of).
 type PostID uint64
+
+//CommentID identifies a comment on a post.
 type CommentID uint64
+
+//ConversationID identifies a conversation.
 type ConversationID uint64
+
+//NotificationID identifies a gleepost notification, eg "John Smith commented on your post!"
 type NotificationID uint64
 
 const (
+	//OSTART - This resource will be retreived starting at an index position ("posts starting from the n-th")
 	OSTART = iota
+	//OBEFORE - This resource will be retreived starting from the entries which happened chronologically right before the index.
 	OBEFORE
+	//OAFTER - Opposite of OBEFORE.
 	OAFTER
 )
 
+//User is the basic user representation, containing their unique ID, their name and their profile image.
 type User struct {
 	ID     UserID `json:"id"`
 	Name   string `json:"name"`
 	Avatar string `json:"profile_image"`
 }
 
+//Profile is the fuller representation of a user, containing their tagline, their primary network, their course and their full name (where available)
 type Profile struct {
 	User
 	Desc     string `json:"tagline"`
@@ -32,17 +51,20 @@ type Profile struct {
 	FullName string `json:"full_name"`
 }
 
+//Contact represents a contact relation from the perspective of a particular user, containing the other user and who has accepted the request so far.
 type Contact struct {
 	User
 	YouConfirmed  bool `json:"you_confirmed"`
 	TheyConfirmed bool `json:"they_confirmed"`
 }
 
+//Network is any grouping of users / posts - ie, a university or a user-created group.
 type Network struct {
 	ID   NetworkID `json:"id"`
 	Name string    `json:"name"`
 }
 
+//Group is a user-group. It's a network with a cover image, a description and maybe a creator.
 type Group struct {
 	Network
 	Image   string `json:"image,omitempty"`
@@ -50,6 +72,8 @@ type Group struct {
 	Creator *User  `json:"creator,omitempty"`
 }
 
+//Message is independent of a conversation. If you need that, see RedisMessage.
+//TODO: Combine them?
 type Message struct {
 	ID   MessageID `json:"id"`
 	By   User      `json:"by"`
@@ -57,22 +81,28 @@ type Message struct {
 	Time time.Time `json:"timestamp"`
 }
 
+//Read represents the most recent message a user has seen in a particular conversation (it doesn't make much sense without that context).
 type Read struct {
 	UserID   UserID    `json:"user"`
 	LastRead MessageID `json:"last_read"`
 }
 
+//RedisMessage is a message with a ConversationID so that someone on the other end of a queue can place it in the correct context.
 type RedisMessage struct {
 	Message
 	Conversation ConversationID `json:"conversation_id"`
 }
 
+//Token is a gleepost access token.
+//TODO: Add scopes?
+//TODO: Deprecate in favour of OAuth?
 type Token struct {
 	UserID UserID    `json:"id"`
 	Token  string    `json:"value"`
 	Expiry time.Time `json:"expiry"`
 }
 
+//PostCore is the minimal representation of a post.
 type PostCore struct {
 	ID   PostID    `json:"id"`
 	By   User      `json:"by"`
@@ -80,6 +110,7 @@ type PostCore struct {
 	Text string    `json:"text"`
 }
 
+//Post represents a slightlly fuller representation of a post, containing everything about a post but its potentially limitless number of comments / likes.
 type Post struct {
 	Network    NetworkID              `json:"-"`
 	ID         PostID                 `json:"id"`
@@ -95,6 +126,7 @@ type Post struct {
 	Group      *Group                 `json:"network,omitempty"`
 }
 
+//PostSmall enhances a Post with a comment count, a like count, and all the users who've liked the post.
 type PostSmall struct {
 	Post
 	CommentCount int        `json:"comment_count"`
@@ -102,6 +134,7 @@ type PostSmall struct {
 	Likes        []LikeFull `json:"likes,omitempty"`
 }
 
+//PostFull enhances a Post with comments and likes.
 type PostFull struct {
 	Post
 	CommentCount int        `json:"comment_count"`
@@ -110,6 +143,7 @@ type PostFull struct {
 	Likes        []LikeFull `json:"likes"`
 }
 
+//Comment is a comment on a Post.
 type Comment struct {
 	ID   CommentID `json:"id"`
 	Post PostID    `json:"-"`
@@ -118,40 +152,48 @@ type Comment struct {
 	Text string    `json:"text"`
 }
 
+//Like represents a user who has liked a post at a particular time.
 type Like struct {
 	UserID UserID
 	Time   time.Time
 }
 
+//LikeFull is the same as a like but contains a whole user object rather than an ID.
 type LikeFull struct {
 	User User      `json:"by"`
 	Time time.Time `json:"timestamp"`
 }
 
+//Rule represents a condition that makes a user part of a particular Network. At the moment the only possible Rule.Type is "email";
+//Rule.Value must then be a domain (eg "gleepost.com") - verified owners of emails in this domain will get added to this network.
 type Rule struct {
 	NetworkID NetworkID
 	Type      string
 	Value     string
 }
 
+//Conversation is a container for a bunch of messages.
 type Conversation struct {
 	ID           ConversationID `json:"id"`
 	LastActivity time.Time      `json:"lastActivity"`
-	Participants []User         `json:"participants"`
-	Read         []Read         `json:"read,omitempty"`
-	Expiry       *Expiry        `json:"expiry,omitempty"`
+	Participants []User         `json:"participants"`     //Participants can send messages to and read from this conversation.
+	Read         []Read         `json:"read,omitempty"`   //Read represents the most recent message each user has seen.
+	Expiry       *Expiry        `json:"expiry,omitempty"` //Expiry is optional; if a conversation does expire, it's no longer accessible.
 }
 
+//ConversationSmall only contains the last message in a conversation - for things like displaying an inbox view.
 type ConversationSmall struct {
 	Conversation
 	LastMessage *Message `json:"mostRecentMessage,omitempty"`
 }
 
+//ConversationAndMessages contains the messages in this conversation.
 type ConversationAndMessages struct {
 	Conversation
 	Messages []Message `json:"messages"`
 }
 
+//MysqlConfig represents the database configuration.
 type MysqlConfig struct {
 	MaxConns int
 	User     string
@@ -160,29 +202,34 @@ type MysqlConfig struct {
 	Port     string
 }
 
+//RedisConfig represents the cache configuration.
 type RedisConfig struct {
 	Proto        string
 	Address      string
-	MessageCache int
-	PostCache    int
-	CommentCache int
+	MessageCache int //Max number of messages per conversation to cache
+	PostCache    int //Max number of posts per network to cache
+	CommentCache int //Max number of comments per post to cache
 }
 
+//AWSConfig contains AWS credentials.
 type AWSConfig struct {
-	KeyId     string
+	KeyID     string
 	SecretKey string
 }
 
+//APNSConfig contains Apple push credentials
 type APNSConfig struct {
 	CertFile   string
 	KeyFile    string
-	Production bool
+	Production bool //Targeting real servers or sandbox?
 }
 
+//GCMConfig contains GCM credentials.
 type GCMConfig struct {
 	APIKey string
 }
 
+//EmailConfig contains SMTP credentials.
 type EmailConfig struct {
 	User       string
 	Pass       string
@@ -192,11 +239,13 @@ type EmailConfig struct {
 	FromHeader string
 }
 
+//FacebookConfig contains facebook credentials.
 type FacebookConfig struct {
 	AppID     string
 	AppSecret string
 }
 
+//Config defines all the available configuration for the API.
 type Config struct {
 	DevelopmentMode      bool
 	Port                 string
@@ -219,12 +268,14 @@ type Config struct {
 	Facebook             FacebookConfig
 }
 
+//Device is a particular (iOS|Android) device owned by a particular user.
 type Device struct {
 	User UserID `json:"user"`
 	Type string `json:"type"`
 	ID   string `json:"id"`
 }
 
+//Notification is a gleepost notification which a user may receive based on other users' actions.
 type Notification struct {
 	ID   NotificationID `json:"id"`
 	Type string         `json:"type"`
@@ -233,79 +284,99 @@ type Notification struct {
 	Seen bool           `json:"seen"`
 }
 
+//PostNotification is a Notification that has "happened" at a particular post.
 type PostNotification struct {
 	Notification
 	Post PostID `json:"post"`
 }
 
+//GroupNotification is a Notification that has "happened" in a particular group.
 type GroupNotification struct {
 	Notification
 	Group NetworkID `json:"network"`
 }
 
+//ConnectionString returns the db/sql string for connecting to MySQL based on this config.
 func (c *MysqlConfig) ConnectionString() string {
 	return c.User + ":" + c.Pass + "@tcp(" + c.Host + ":" + c.Port + ")/gleepost?charset=utf8"
 }
 
+//APIerror is a JSON-ified error.
 type APIerror struct {
 	Reason string `json:"error"`
 }
 
+//Created is a convenience structure for when you just want to indicate the id of some created resource.
 type Created struct {
 	ID uint64 `json:"id"`
 }
 
+//NewUser represents the status of a user as part of the registration process.
 type NewUser struct {
 	ID     UserID `json:"id"`
 	Status string `json:"status"`
 }
 
+//URLCreated represents a url you've uploaded.
 type URLCreated struct {
 	URL string `json:"url"`
 }
 
+//BusyStatus is an indication of whether the user is Busy (accepting random chats) or not.
 type BusyStatus struct {
 	Busy bool `json:"busy"`
 }
 
+//Liked represents a particular post and whether you've liked it.
 type Liked struct {
 	Post  PostID `json:"post"`
 	Liked bool   `json:"liked"`
 }
 
+//CategoryID identifies a particular post category/tag.
 type CategoryID uint64
 
+//PostCategory represents a particular post category.
 type PostCategory struct {
 	ID   CategoryID `json:"id"`
 	Tag  string     `json:"tag"`
 	Name string     `json:"name"`
 }
 
+//Expiry indicates when a conversation is due to expire / whether it has ended yet.
 type Expiry struct {
 	Time  time.Time `json:"time"`
 	Ended bool      `json:"ended"`
 }
 
+//NewExpiry creates an expiry d into the future.
 func NewExpiry(d time.Duration) *Expiry {
 	return &Expiry{Time: time.Now().Add(d), Ended: false}
 }
 
+//Error - implements the error interface.
 func (e APIerror) Error() string {
 	return e.Reason
 }
 
+//ENOSUCHUSER is the error that should be returned when performing some action against a non-existent user.
 var ENOSUCHUSER = APIerror{"No such user."}
 
+//MsgQueue will deliver you a bunch of json-encoded things (internal events or messages sent to the user) through MsgQueue.Messages.
+//You can stop listening by sending QueueCommand{"UNSUBSCRIBE", ""} and after a little while the Messages chan should close.
 type MsgQueue struct {
 	Commands chan QueueCommand
 	Messages chan []byte
 }
 
+//QueueCommand represents a command to be sent to the message queue. It's sent as is, so never expose this to API clients!
 type QueueCommand struct {
 	Command string
 	Value   string
 }
 
+//Event represents something that happened which a consumer of a MsgQueue wants to hear about in real time.
+//It has a type, a location (typically a resource) and a json payload.
 type Event struct {
 	Type     string      `json:"type"`
 	Location string      `json:"location,omitempty"`
