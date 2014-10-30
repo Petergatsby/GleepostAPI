f728bf200f91c8e73adcf7a7c1cf6295882e941a
Patrick Molgaard <patrick@gleepost.com>
2014-10-30 13:52:39 -0700

    Script to work out which post images I broke...

---
+++ scripts/catch_404s.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	"github.com/draaglom/GleepostAPI/lib/conf"
+)
+
+func main() {
+	conf := conf.GetConfig()
+	database, err := sql.Open("mysql", conf.Mysql.ConnectionString())
+	if err != nil {
+		return
+	}
+
+	urlsStmt, err := database.Prepare("SELECT url FROM post_images")
+	if err != nil {
+		return
+	}
+	rows, err := urlsStmt.Query()
+	if err != nil {
+		return
+	}
+	defer rows.Close()
+	i := 0
+	for rows.Next() {
+		var url string
+		err := rows.Scan(&url)
+		if err != nil {
+			return
+		}
+		_, err = http.Get(url)
+		if err != nil {
+			i++
+		}
+
+	}
+	fmt.Println(i, "urls broke (should be 88)")
+}
