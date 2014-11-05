20b933cafa1827061b89f8c9439a35f374d4a6a4
Patrick Molgaard <patrick@gleepost.com>
2014-11-04 16:58:05 -0800

    Define network approval settings columns

---
+++ db/migrations/20141104165143_ApprovalSetting.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"database/sql"
+)
+
+// Up is executed when this migration is applied
+func Up_20141104165143(txn *sql.Tx) {
+	_, err := txn.Query("ALTER TABLE network ADD approval_level INT(5) UNSIGNED")
+	if err != nil {
+		txn.Rollback()
+	}
+	_, err = txn.Query("ALTER TABLE network ADD approved_categories VARCHAR(255) NULL")
+	if err != nil {
+		txn.Rollback()
+	}
+}
+
+// Down is executed when this migration is rolled back
+func Down_20141104165143(txn *sql.Tx) {
+	_, err := txn.Query("ALTER TABLE network DROP COLUMN approval_level")
+	if err != nil {
+		txn.Rollback()
+	}
+	_, err := txn.Query("ALTER TABLE network DROP COLUMN approved_categories")
+	if err != nil {
+		txn.Rollback()
+	}
+}
