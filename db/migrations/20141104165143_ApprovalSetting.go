aea3ba34a49c26a5ae3c3c68065f81e229fd6178
Patrick Molgaard <patrick@gleepost.com>
2014-11-05 15:15:40 -0800

    Level is not null

--- db/migrations/20141104165143_ApprovalSetting.go
+++ db/migrations/20141104165143_ApprovalSetting.go
@@ -6,7 +6,7 @@ import (
 
 // Up is executed when this migration is applied
 func Up_20141104165143(txn *sql.Tx) {
-	_, err := txn.Query("ALTER TABLE network ADD approval_level INT(5) UNSIGNED")
+	_, err := txn.Query("ALTER TABLE network ADD approval_level INT(5) UNSIGNED NOT NULL DEFAULT 0")
 	if err != nil {
 		txn.Rollback()
 	}
