abe1cb35c4ed6e254faeb0a4c6f06ccdf0e7194f
Patrick Molgaard <patrick@gleepost.com>
2014-11-20 16:09:20 -0800

    Switch profile images to CDN

---
+++ db/migrations/20141120154940_profileImagesInCDN.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"database/sql"
+)
+
+// Up is executed when this migration is applied
+func Up_20141120154940(txn *sql.Tx) {
+	_, err := txn.Query("UPDATE users SET avatar = CONCAT('http://d2tc2ce3464r63.cloudfront.net', SUBSTR(avatar, 41)) WHERE avatar LIKE '%gpimg%'")
+	if err != nil {
+		txn.Rollback()
+		return
+	}
+	_, err = txn.Query("UPDATE users SET avatar = CONCAT('http://d3itv2rmlfeij9.cloudfront.net', SUBSTR(avatar, 42)) WHERE avatar LIKE '%gpcali%'")
+	if err != nil {
+		txn.Rollback()
+		return
+	}
+}
+
+// Down is executed when this migration is rolled back
+func Down_20141120154940(txn *sql.Tx) {
+	_, err := txn.Query("UPDATE users SET avatar = CONCAT('https://s3-eu-west-1.amazonaws.com/gpimg', SUBSTR(avatar, 37)) WHERE avatar LIKE '%d2tc2ce3464r63%'")
+	if err != nil {
+		txn.Rollback()
+		return
+	}
+	_, err = txn.Query("UPDATE users SET avatar = CONCAT('https://s3-us-west-1.amazonaws.com/gpcali', SUBSTR(avatar, 37)) WHERE avatar LIKE '%d3itv2rmlfeij9%'")
+	if err != nil {
+		txn.Rollback()
+		return
+	}
+}
