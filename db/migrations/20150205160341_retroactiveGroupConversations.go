5c06a907ad431efc38234f9bb1656758078ff07c
Patrick Molgaard <patrick@gleepost.com>
2015-02-05 17:14:59 -0800

    Fix retroactive group conversation creation

--- db/migrations/20150205160341_retroactiveGroupConversations.go
+++ db/migrations/20150205160341_retroactiveGroupConversations.go
@@ -7,12 +7,12 @@ import (
 
 // Up is executed when this migration is applied
 func Up_20150205160341(txn *sql.Tx) {
-	_, err := txn.Query("INSERT INTO conversations (initiator, last_mod, primary_conversation, group_id) VALUES (SELECT creator, NOW(), false, id FROM network WHERE creator IS NOT NULL)")
+	_, err := txn.Query("INSERT INTO conversations (initiator, last_mod, primary_conversation, group_id) SELECT creator, NOW(), false, id FROM network WHERE creator IS NOT NULL")
 	if err != nil {
 		log.Println(err)
 		txn.Rollback()
 	}
-	_, err = txn.Query("INSERT INTO conversation_participants (conversation_id, participant_id) VALUES (SELECT conversations.group_id, user_network.user_id FROM conversations JOIN user_network ON conversations.group_id = user_network.network_id)")
+	_, err = txn.Query("INSERT INTO conversation_participants (conversation_id, participant_id) SELECT conversations.id, user_network.user_id FROM conversations JOIN user_network ON conversations.group_id = user_network.network_id")
 	if err != nil {
 		log.Println(err)
 		txn.Rollback()
