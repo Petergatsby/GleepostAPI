6a315ecb24f08351d84db9124391aedf98ffc511
Patrick Molgaard <patrick@gleepost.com>
2015-02-18 13:34:03 -0800

    Delete old comments table

---
+++ db/migrations/20150218132731_DeleteOldCommentsTable.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+)
+
+// Up is executed when this migration is applied
+func Up_20150218132731(txn *sql.Tx) {
+	_, err := txn.Query("DROP TABLE comments")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+}
+
+// Down is executed when this migration is rolled back
+func Down_20150218132731(txn *sql.Tx) {
+	q := "CREATE TABLE `comments` ( " +
+		"`id` int(10) unsigned NOT NULL AUTO_INCREMENT, " +
+		"`user_id` int(10) unsigned NOT NULL, " +
+		"`listing_id int(10) unsigned NOT NULL, " +
+		"`text` varchar(512) NOT NULL, " +
+		"`timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, " +
+		"PRIMARY KEY (`id`) ) " +
+		"ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;"
+	_, err := txn.Query("q")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
