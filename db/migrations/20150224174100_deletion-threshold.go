736f9c343d2208c42ab361203c600c7f4a22b8fa
Patrick Molgaard <patrick@gleepost.com>
2015-02-24 17:42:36 -0800

    Rename migration so it can actually be performed

--- db/migrations/20150224174100_deletion-threshold.go
+++ db/migrations/20150224174100_deletion-threshold.go
@@ -6,7 +6,7 @@ import (
 )
 
 // Up is executed when this migration is applied
-func Up_20150204183513(txn *sql.Tx) {
+func Up_20150224174100(txn *sql.Tx) {
 	_, err := txn.Query("ALTER TABLE conversation_participants ADD deletion_threshold INT(10) UNSIGNED NOT NULL DEFAULT 0")
 	if err != nil {
 		log.Println(err)
@@ -16,11 +16,10 @@ func Up_20150204183513(txn *sql.Tx) {
 }
 
 // Down is executed when this migration is rolled back
-func Down_20150204183513(txn *sql.Tx) {
+func Down_20150224174100(txn *sql.Tx) {
 	_, err := txn.Query("ALTER TABLE conversation_participants DROP COLUMN deletion_threshold")
 	if err != nil {
 		log.Println(err)
 		txn.Rollback()
 	}
-
 }
