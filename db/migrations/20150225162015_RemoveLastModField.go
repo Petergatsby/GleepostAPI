57c5ff126460b00733c157770a5b3532fd0b7b27
Patrick Molgaard <patrick@gleepost.com>
2015-02-25 16:23:32 -0800

    Remove last_mod from conversations table

---
+++ db/migrations/20150225162015_RemoveLastModField.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+)
+
+// Up is executed when this migration is applied
+func Up_20150225162015(txn *sql.Tx) {
+	_, err := txn.Query("ALTER TABLE conversations DROP COLUMN last_mod")
+	if err != nil {
+		log.Println(err)
+		txn.Rollback()
+	}
+}
+
+// Down is executed when this migration is rolled back
+func Down_20150225162015(txn *sql.Tx) {
+	_, err := txn.Query("ALTER TABLE conversations ADD last_mod TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP")
+	if err != nil {
+		log.Println(err)
+		txn.Rollback()
+	}
+}
