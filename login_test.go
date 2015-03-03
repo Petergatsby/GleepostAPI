b1ba6b742c88e68af7853bef5a19a121bda6abdd
Patrick Molgaard <patrick@gleepost.com>
2015-03-02 16:23:56 -0800

    Add registration test; login remains broken until I can verify the registered user.

--- login_test.go
+++ login_test.go
@@ -12,11 +12,64 @@ import (
 	"github.com/draaglom/GleepostAPI/lib/gp"
 )
 
-func TestLogin(t *testing.T) {
+var baseUrl = "http://localhost:8083/api/v1/"
+
+func TestInit(t *testing.T) {
+	//Init
 	err := initDB()
 	if err != nil {
 		t.Fatalf("Error initializing db: %v\n", err)
 	}
+	go main()
+	time.Sleep(500 * time.Millisecond) //Time to spin up
+}
+
+func TestRegister(t *testing.T) {
+	data := make(url.Values)
+	client := &http.Client{}
+	data["email"] = []string{"patrick@fakestanford.edu"}
+	data["pass"] = []string{"TestingPass"}
+	data["first"] = []string{"Patrick"}
+	data["last"] = []string{"Molgaard"}
+	resp, err := client.PostForm(baseUrl+"register", data)
+	if err != nil {
+		t.Fatalf("Error making http request: %v\n", err)
+	}
+	if resp.StatusCode != http.StatusCreated {
+		t.Fatalf("Wrong status code: Got %v but was expecting %v", resp.StatusCode, http.StatusCreated)
+	}
+	created := gp.NewUser{}
+	dec := json.NewDecoder(resp.Body)
+	err = dec.Decode(&created)
+	if err != nil {
+		t.Fatalf("Error parsing registration response: %v\n", err)
+	}
+	if created.Status != "unverified" {
+		t.Fatalf("Status should be 'unverified', but is actually: %s\n", created.Status)
+	}
+}
+
+func TestLoginBadPass(t *testing.T) {
+	//Bad password
+	resp, err := loginRequest("patrick@fakestanford.edu", "bad pass")
+	if err != nil {
+		t.Fatalf("Error logging in: %v\n", err)
+	}
+	if resp.StatusCode != 400 {
+		t.Fatalf("Got status code %d, expected %d\n", resp.StatusCode, 400)
+	}
+	errorValue := gp.APIerror{}
+	dec := json.NewDecoder(resp.Body)
+	err = dec.Decode(&errorValue)
+	if err != nil {
+		t.Fatalf("Error parsing error: %v\n", err)
+	}
+	if errorValue.Reason != "Bad username/password" {
+		t.Fatalf("Expected %s, got %s\n", "Bad username/password", errorValue.Reason)
+	}
+}
+
+func TestLoginGood(t *testing.T) {
 	//Good user
 	email := "patrick@fakestanford.edu"
 	pass := "TestingPass"
@@ -46,24 +99,6 @@ func TestLogin(t *testing.T) {
 	if token.Expiry.AddDate(-1, 0, 0).Before(time.Now().Add(-1 * time.Minute)) {
 		t.Fatalf("Token expiration shorter than it should be!")
 	}
-	//Bad password
-	resp, err = loginRequest(email, "bad pass")
-	if err != nil {
-		t.Fatalf("Error logging in: %v\n", err)
-	}
-	if resp.StatusCode != 400 {
-		t.Fatalf("Got status code %d, expected %d\n", resp.StatusCode, 400)
-	}
-	errorValue := gp.APIerror{}
-	dec = json.NewDecoder(resp.Body)
-	err = dec.Decode(&errorValue)
-	if err != nil {
-		t.Fatalf("Error parsing error: %v\n", err)
-	}
-	if errorValue.Reason != "Bad username/password" {
-		t.Fatalf("Expected %s, got %s\n", "Bad username/password", errorValue.Reason)
-	}
-
 }
 
 func initDB() error {
@@ -80,7 +115,27 @@ func initDB() error {
 	if err != nil {
 		return err
 	}
-	_, err = stmt.Exec("Fake Stanford", true, true)
+	res, err := stmt.Exec("Fake Stanford", true, true)
+	if err != nil {
+		return err
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		return err
+	}
+	_, err = db.Exec("TRUNCATE TABLE `net_rules`")
+	if err != nil {
+		return err
+	}
+	stmt, err = db.Prepare("INSERT INTO `net_rules` (network_id, rule_type, rule_value) VALUES (?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	_, err = stmt.Exec(id, "email", "fakestanford.edu")
+	if err != nil {
+		return err
+	}
+	_, err = db.Exec("TRUNCATE TABLE `users`")
 	if err != nil {
 		return err
 	}
@@ -88,7 +143,6 @@ func initDB() error {
 }
 
 func loginRequest(email, pass string) (resp *http.Response, err error) {
-	baseUrl := "https://dev.gleepost.com/api/v1/"
 	data := make(url.Values)
 	client := &http.Client{}
 	data["email"] = []string{email}
