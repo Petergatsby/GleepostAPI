bb57bd7aedf58c687d78318ab47b36a8d0a75bdf
Patrick Molgaard <patrick@gleepost.com>
2015-03-06 15:39:19 -0800

    Move admin-ship from network to flag on username

---
+++ db/migrations/20150306153027_SetNetworkFlag.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/draaglom/GleepostAPI/lib/conf"
+)
+
+// Up is executed when this migration is applied
+func Up_20150306153027(txn *sql.Tx) {
+	conf := conf.GetConfig()
+	s, err := txn.Prepare("UPDATE users SET is_admin = 1 WHERE id IN (SELECT user_id FROM user_network WHERE network_id = ?)")
+	if err != nil {
+		log.Println(err)
+		txn.Rollback()
+		return
+	}
+	_, err = s.Exec(conf.Admins)
+	if err != nil {
+		log.Println(err)
+		txn.Rollback()
+		return
+	}
+}
+
+// Down is executed when this migration is rolled back
+func Down_20150306153027(txn *sql.Tx) {
+	_, err := txn.Query("UPDATE users SET is_admin = 0")
+	if err != nil {
+		log.Println(err)
+		txn.Rollback()
+		return
+	}
+}
