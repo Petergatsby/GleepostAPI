ed3fd888b36c4530528917f692cd437d49b28a2c
Patrick Molgaard <patrick@gleepost.com>
2015-03-27 17:12:43 +0000

    More db boilerplate for viewing/editing templates

--- lib/db/post-templates.go
+++ lib/db/post-templates.go
@@ -19,3 +19,45 @@ func (db *DB) CreateTemplate(group gp.TemplateGroupID, template string) (id gp.T
 	id = gp.TemplateID(_id)
 	return
 }
+
+//GetTemplate returns a specific template.
+func (db *DB) GetTemplate(id gp.TemplateID) (template string, err error) {
+	s, err := db.prepare("SELECT template FROM post_templates WHERE id = ?")
+	if err != nil {
+		return
+	}
+	err = s.QueryRow(id).Scan(&template)
+	return
+}
+
+//GetTemplateSet returns all the post templates in this set.
+func (db *DB) GetTemplateSet(set gp.TemplateGroupID) (templates []string, err error) {
+	s, err := db.prepare("SELECT template FROM post_templates WHERE set = ?")
+	if err != nil {
+		return
+	}
+	rows, err := s.Query(set)
+	if err != nil {
+		return
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var tmpl string
+		err = rows.Scan(&tmpl)
+		if err != nil {
+			return
+		}
+		templates = append(templates, tmpl)
+	}
+	return
+}
+
+//UpdateTemplate saves a new Template
+func (db *DB) UpdateTemplate(id gp.TemplateID, group gp.TemplateGroupID, template string) (err error) {
+	s, err := db.prepare("REPLACE INTO post_templates (id, set, template) VALUES (?, ?, ?)")
+	if err != nil {
+		return
+	}
+	_, err = s.Exec(id, group, template)
+	return
+}
