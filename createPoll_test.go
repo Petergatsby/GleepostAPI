46a4980f4132e0f60419337be075c133c594e14d
Patrick Molgaard <patrick@gleepost.com>
2015-04-01 17:35:02 +0100

    Expand poll creation test

--- createPoll_test.go
+++ createPoll_test.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"strings"
 	"testing"
 	"time"
+
+	"github.com/draaglom/GleepostAPI/lib/gp"
 )
 
 func TestCreatePoll(t *testing.T) {
@@ -11,9 +18,17 @@ func TestCreatePoll(t *testing.T) {
 		t.Fatalf("Error initializing db: %v\n", err)
 	}
 
+	token, err := testingGetSession("patrick@fakestanford.edu", "TestingPass")
+	if err != nil {
+		t.Fatalf("Couldn't log in: %v\n", err)
+	}
+
+	client := &http.Client{}
+
 	type createPollTest struct {
-		Email              string
-		Pass               string
+		Token              gp.Token
+		Text               string
+		Categories         []string
 		PollOptions        []string
 		PollExpiry         string
 		ExpectedStatusCode int
@@ -21,16 +36,46 @@ func TestCreatePoll(t *testing.T) {
 		ExpectedError      string
 	}
 	testGood := createPollTest{
-		Email:              "patrick@fakestanford.edu",
-		Pass:               "TestingPass",
+		Token:              token,
+		Text:               "Which is the best option?",
+		Categories:         []string{"poll"},
 		PollOptions:        []string{"Option 1", "Another option", "Nothing"},
-		PollExpiry:         time.Now().Add(24 * time.Hour).String(),
+		PollExpiry:         time.Now().Add(24 * time.Hour).Format(time.RFC3339),
 		ExpectedStatusCode: 201,
-		ExpectedType:       "Created",
+		ExpectedType:       "CreatedPost",
 	}
 
 	tests := []createPollTest{testGood}
 	for _, cpt := range tests {
+		data := make(url.Values)
+		data["id"] = []string{fmt.Sprintf("%d", cpt.Token.UserID)}
+		data["token"] = []string{cpt.Token.Token}
+		data["text"] = []string{cpt.Text}
+		data["poll-options"] = []string{strings.Join(cpt.PollOptions, ",")}
+		data["poll-expiry"] = []string{cpt.PollExpiry}
+		data["categories"] = cpt.Categories
 
+		resp, err := client.PostForm(baseURL+"posts", data)
+		if err != nil {
+			t.Fatal("Error making request:", err)
+		}
+		if resp.StatusCode != cpt.ExpectedStatusCode {
+			t.Fatalf("Expected status code %d, got %d\n", cpt.ExpectedStatusCode, resp.StatusCode)
+		}
+		dec := json.NewDecoder(resp.Body)
+		switch {
+		case cpt.ExpectedType == "CreatedPost":
+			post := gp.CreatedPost{}
+			err = dec.Decode(&post)
+			if err != nil {
+				t.Fatalf("Failed to decode as %s: %v\n", cpt.ExpectedType, err)
+			}
+			if post.ID < 1 {
+				t.Fatalf("Post.ID must be nonzero (%d)\n", post.ID)
+			}
+			if post.Pending == true {
+				t.Fatalf("Post should not be pending")
+			}
+		}
 	}
 }
