dd320becb519e7191134f7b952eecdf877afa44c
Patrick Molgaard <patrick@gleepost.com>
2015-04-02 18:57:31 +0100

    Split device types into own file

--- lib/gp/types.go
+++ lib/gp/types.go
@@ -71,13 +71,6 @@ type ReviewEvent struct {
 	At     time.Time `json:"at"`
 }
 
-//Device is a particular (iOS|Android) device owned by a particular user.
-type Device struct {
-	User UserID `json:"user"`
-	Type string `json:"type"`
-	ID   string `json:"id"`
-}
-
 //APIerror is a JSON-ified error.
 type APIerror struct {
 	Reason string `json:"error"`
