3c47f7dae70a0d7f383b4656a7e731b7c98a75fd
Patrick Molgaard <patrick@gleepost.com>
2015-04-03 16:31:15 +0100

    Add db.SavePoll()

---
+++ lib/db/polls.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"time"
+
+	"github.com/draaglom/GleepostAPI/lib/gp"
+)
+
+//SavePoll adds this poll to this post.
+func (db *DB) SavePoll(postID gp.PostID, pollExpiry time.Time, pollOptions []string) {
+	s, err := db.prepare("INSERT INTO post_polls (post_id, expiry_time) VALUES (?, ?)")
+	if err != nil {
+		return
+	}
+	_, err = s.Exec(postID, pollExpiry.Format(mysqlTime))
+	if err != nil {
+		return
+	}
+	s, err = db.prepare("INSERT INTO poll_options (post_id, option_id, option) VALUES (?, ?, ?)")
+	if err != nil {
+		return
+	}
+	for i, opt := range pollOptions {
+		_, err = s.Exec(postID, i, opt)
+		if err != nil {
+			return
+		}
+	}
+}
