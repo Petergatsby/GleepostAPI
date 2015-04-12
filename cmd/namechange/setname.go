eddaf9722a2701e10633a8abc315a121038a9e44
Patrick Molgaard <patrick@gleepost.com>
2015-04-12 23:55:40 +0100

    Update setname command to take project root (or pbxproj) as arg

--- cmd/namechange/setname.go
+++ cmd/namechange/setname.go
@@ -6,12 +6,26 @@ import (
 	"io/ioutil"
 	"os"
 	"regexp"
+
+	"github.com/draaglom/xctools/xcassets"
 )
 
 func main() {
 	flag.Parse()
 	path := flag.Arg(0)
-	pbxproj, err := ioutil.ReadFile(path)
+	found, err := xcassets.FindPath(path, "project.pbxproj")
+	if err != nil || len(found) == 0 {
+		fmt.Println("Couldn't find project.pbxproj in this tree:", dest)
+		os.Exit(-1)
+	}
+	if len(found) > 1 {
+		fmt.Println("Destination ambiguous: Found multiple project.pbxproj in this tree:")
+		for _, f := range found {
+			fmt.Println(f)
+		}
+		os.Exit(-1)
+	}
+	pbxproj, err := ioutil.ReadFile(found[0])
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
