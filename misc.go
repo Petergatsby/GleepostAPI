4ebed363acf032fddfc4e36dabe78008254c6661
Patrick Molgaard <patrick@gleepost.com>
2015-04-16 23:58:15 +0100

    Allow X-GP-Auth header in OPTIONS

--- misc.go
+++ misc.go
@@ -36,6 +36,7 @@ func optionsHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Access-Control-Allow-Headers", "X-GP-Auth")
 	w.WriteHeader(200)
 }
 
