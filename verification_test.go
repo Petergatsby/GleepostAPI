68394482c54e68994bfeb714acf6bcd589a65382
Patrick Molgaard <patrick@gleepost.com>
2015-04-24 15:15:15 +0100

    Simplify verification test a bit

--- verification_test.go
+++ verification_test.go
@@ -76,29 +76,21 @@ func TestVerification(t *testing.T) {
 			t.Fatalf("Test%v: Error making http request: %v\n", testNumber, err)
 		}
 
+		var token string
 		if vt.TestValidToken {
-			var token string
-			err = db.QueryRow("SELECT token FROM verification JOIN users ON users.id = verification.user_id WHERE users.email = ?", vt.Email).Scan(&token)
-
+			token, err = getToken(db, vt.Email)
 			if err != nil {
 				t.Fatalf("Test%v: Error finding token: %v\n", testNumber, err)
 			}
-			if token == "" {
-				t.Fatalf("Test%v: Incorrect token retrieved: %v\n", testNumber, token)
-			}
-
-			resp, err = client.PostForm(baseURL+"verify/"+token, make(url.Values))
-			if err != nil {
-				t.Fatalf("Test%v: Error with verification request: %v\n", testNumber, err)
-			}
-			if vt.VerifyTwice {
-				resp, err = client.PostForm(baseURL+"verify/"+token, make(url.Values))
-				if err != nil {
-					t.Fatalf("Test%v: Error with verification request: %v\n", testNumber, err)
-				}
-			}
 		} else {
-			resp, err = client.PostForm(baseURL+"verify/12345lolololtest", make(url.Values))
+			token = "12345lolololtest"
+		}
+		resp, err = client.PostForm(baseURL+"verify/"+token, make(url.Values))
+		if err != nil {
+			t.Fatalf("Test%v: Error with verification request: %v\n", testNumber, err)
+		}
+		if vt.VerifyTwice {
+			resp, err = client.PostForm(baseURL+"verify/"+token, make(url.Values))
 			if err != nil {
 				t.Fatalf("Test%v: Error with verification request: %v\n", testNumber, err)
 			}
@@ -130,4 +122,9 @@ func TestVerification(t *testing.T) {
 			t.Fatalf("Test%v: Should not have been able to log in.", testNumber)
 		}
 	}
+}
+
+func getToken(db *sql.DB, email string) (token string, err error) {
+	err = db.QueryRow("SELECT token FROM verification JOIN users ON users.id = verification.user_id WHERE users.email = ?", email).Scan(&token)
+	return
 }
