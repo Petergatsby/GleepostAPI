3568c8af48886a01f467cc2eb849045ee6116019
Patrick Molgaard <patrick@gleepost.com>
2015-08-26 14:34:29 +0100

    add dummy data

--- lib/search-messages.go
+++ lib/search-messages.go
@@ -1,6 +1,10 @@
 package lib
 
-import "github.com/draaglom/GleepostAPI/lib/gp"
+import (
+	"time"
+
+	"github.com/draaglom/GleepostAPI/lib/gp"
+)
 
 type MessageResult struct {
 	Messages []MatchedMessage `json:"messages"`
@@ -12,5 +16,13 @@ type MatchedMessage struct {
 }
 
 func (api *API) SearchMessagesInConversation(userID gp.UserID, convID gp.ConversationID, query string, mode int, index int64) (hits []MessageResult, err error) {
+	hits = []MessageResult{
+		{
+			Messages: []MatchedMessage{
+				{Message: gp.Message{ID: 1, Text: "sup", By: gp.User{ID: 1, Name: "patrick"}, Time: time.Now().UTC().Add(-1 * time.Minute)}, Matched: true},
+				{Message: gp.Message{ID: 2, Text: "nm u?", By: gp.User{ID: 2, Name: "tade"}, Time: time.Now().UTC()}},
+			},
+		},
+	}
 	return hits, nil
 }
