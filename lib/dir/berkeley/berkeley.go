a0e620298590840da5d5b3ca8266d1a049ccb6b1
Patrick Molgaard <patrick@gleepost.com>
2015-09-28 16:18:12 +0100

    remove logging

--- lib/dir/berkeley/berkeley.go
+++ lib/dir/berkeley/berkeley.go
@@ -122,7 +122,6 @@ func parseIndividualResult(doc *goquery.Document) (result Member, err error) {
 			for _, line := range newLines {
 				addressLines = append(addressLines, strings.TrimSpace(line))
 			}
-			log.Printf("%q\n", strings.TrimSpace(node.Data))
 		case node.Type == html.ElementNode && node.Data == "a" && node.FirstChild != nil && lastLabel == "Email":
 			result.Email = node.FirstChild.Data
 		case node.Type == html.ElementNode && node.Data == "a" && node.FirstChild != nil && lastLabel == "Website":
