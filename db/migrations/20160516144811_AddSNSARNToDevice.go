1e6bb219b53fa35b6134cec67957116ea2d7eccc
Patrick Molgaard <patrick@gleepost.com>
2016-05-17 00:00:38 +0100

    Add nullable arn column to device table

---
+++ db/migrations/20160516144811_AddSNSARNToDevice.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+)
+
+// Up is executed when this migration is applied
+func Up20160516144811(txn *sql.Tx) {
+	_, err := txn.Query("ALTER TABLE devices ADD arn VARCHAR(100) NULL")
+	if err != nil {
+		log.Println(err)
+		txn.Rollback()
+	}
+}
+
+// Down is executed when this migration is rolled back
+func Down20160516144811(txn *sql.Tx) {
+	_, err := txn.Query("ALTER TABLE devices DROP COLUMN arn")
+	if err != nil {
+		log.Println(err)
+		txn.Rollback()
+	}
+}
